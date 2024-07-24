b30bb42f95c62444500d5d60cca0e98fec3b2640
Nisha Yadav <nisha.yadavsep03@gmail.com>
2024-07-24 12:34:55 +0530

    dictionary , nested, average

---
+++ pattern4.py
@@ -0,0 +1,14 @@
+from turtle import *
+speed('fast')
+pencolor('yellow')
+bgcolor('black')
+total = 0
+while total <100:
+    total +=5
+    lt(90)
+total = 0
+while total < 1000:
+    total+= 5
+    fd(total)
+    lt(59)
+mainloop()
